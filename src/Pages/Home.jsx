836859dc26864cf4b2718305de8c391a2f77a878
Dio Siregar <dio.siregar03@gmail.com>
2023-10-27 09:36:58 +0700

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,10 +6,11 @@ import CardBestseller from '../Component/CardBestseller/CardBestseller'
 import axios from 'axios'
 import { getAuth } from 'firebase/auth'
 import bestSellerIMG from '../assets/BestSeller.png'
+import fireConfig from '../Config/firebase'
 
 const Home = () => {
   useEffect(() => {
-    const auth = getAuth()
+    const auth = getAuth(fireConfig)
     const fetchData = async () => {
         try {
             const response = await axios.get('http://localhost:8080/api/v1/customer');
